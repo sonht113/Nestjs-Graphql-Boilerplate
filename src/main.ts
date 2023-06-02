ef28c8d6ebe210b858fb270a727d9de4cc663bdd
sonht113 <trongson18101999>
2023-06-03 01:39:14 +0700

    Update upload file and Fix version package graphql-upload@14 -> @13.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(
+    graphqlUploadExpress({
+      maxFiles: 10,
+      maxFileSize: 1000000000,
+    }),
+  );
 
   await app.listen(3000);
 }
